eb3eeedb0943d631f272114f06c1e5905147ea7c
Javier Gil <FJavier.GilR@GMail.com>
2015-10-23 22:11:39 +0200

    Inicio Proyecto

---
+++ app/src/main/java/es/upm/miw/ficheros/FicherosActivity.java
@@ -0,0 +1,13 @@
+package es.upm.miw.ficheros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FicherosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ficheros);
+    }
+}
